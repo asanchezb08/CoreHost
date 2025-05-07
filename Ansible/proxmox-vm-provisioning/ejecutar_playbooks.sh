333eee044da0c2841477eb4cb5a4870389c3f508
Albert <albert.sanchez1511@gmail.com>
2025-05-07 16:04:11 +0200

    Solo Ansible

---
+++ Ansible/proxmox-vm-provisioning/ejecutar_playbooks.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+export HOME=/home/ansible
+export USER=ansible
+
+eval "$(ssh-agent -s)" >/dev/null 2>&1
+ssh-add /home/ansible/.ssh/id_rsa >/dev/null 2>&1
+
+# Cambiado a una ruta donde sí tienes permiso
+LOGFILE="/home/ansible/ansible_minuto.log"
+echo "--- $(date): Ejecutando playbooks ---" >> "$LOGFILE"
+
+for playbook in /home/ansible/proxmox-vm-provisioning/playbooks/*.yml; do
+  echo "Iniciando $playbook" >> "$LOGFILE"
+  /usr/bin/ansible-playbook "$playbook" >> "$LOGFILE" 2>&1
+done
